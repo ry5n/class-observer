aab1b5c751e9a0ab5aaf196e14e5d17a6732a8a4
Ryan Frederick <ryan@ryanfrederick.com>
2014-03-31 11:11:53 -0700

    Allow beacon prop’s from and to values to be configured.

--- index.js
+++ index.js
@@ -38,7 +38,8 @@
   }
 
   /**
-   * Build a unique stylesheet for an observed element’s current state.
+   * Render stylesheet that will trigger an event when the specified element’s
+   * class attribute changes.
    *
    * The CSS specifies two style rules: the first on the element sets a dummy
    * transition on the provided propery with a near-zero duration. The second
@@ -46,24 +47,38 @@
    * attribute other than its current value. Hence, a transitionend even will
    * be fired on the element whenever the class changes.
    *
-   * @param  {[type]} id        [description]
-   * @param  {[type]} prop      [description]
-   * @param  {[type]} classAttr [description]
-   * @return {[type]}           [description]
+   * @param {Number}  id
+   *   Id of the target instance
+   *
+   * @param {Object}  config
+   *   Beacon config object with the following keys:
+   *   - property: the CSS property to transition
+   *   - from: the initial value
+   *   - to: the final value
+   *
+   * @param {String}  currentClass
+   *   The current value of the observed element’s class attriute
+   *
+   * @return {String}
+   *   Rendered beacon stylesheet
    */
 
-  function getBeaconStyle(id, prop, classAttr) {
-    var template = '\
-      [data-class-observee="' + id + '"] {\
-        -webkit-transition: ' + prop + ' 1ms;\
-        -moz-transition: ' + prop + ' 1ms;\
-        -o-transition: ' + prop + ' 1ms;\
-        transition: ' + prop + ' 1ms;\
-      }\
-      [data-class-observee="' + id + '"]:not([class="' + classAttr + '"]) {\
-        ' + prop + ': hsla(0, 0%, 100%, 0);\
-      }\
-    ';
+  function renderBeacon(id, config, currentClass) {
+    var prop = config.property,
+        from = config.from,
+        to = config.to,
+        template = '\
+          [data-class-observee="' + id + '"] {\
+            ' + prop + ': ' + from + ';\
+            -webkit-transition: ' + prop + ' 1ms;\
+            -moz-transition: ' + prop + ' 1ms;\
+            -o-transition: ' + prop + ' 1ms;\
+            transition: ' + prop + ' 1ms;\
+          }\
+          [data-class-observee="' + id + '"]:not([class="' + currentClass + '"]) {\
+            ' + prop + ': ' + to + ';\
+          }\
+        ';
 
     return template;
   }
@@ -72,17 +87,28 @@
    * Create a ClassObserver instance that watches for changes to an element’s
    * class attribute.
    *
-   * @param {Element} el          Element to observe for class changes
-   * @param {String}  beaconProp  CSS property to use
+   * @param {Element}  el
+   *   Element to observe for class changes
+   *
+   * @param {Object}  beaconConfig
+   *   Configuration for the observer’s internal beacon stylesheet, with the
+   *   following keys:
+   *   - property: the CSS property to transition
+   *   - from: the initial value
+   *   - to: the final value
    */
 
-  function ClassObserver(el, beaconProp) {
+  function ClassObserver(el, beaconConfig) {
     this.el = el,
     this.oldClass = '',
     this.currentClass = '',
     this.id = newInstanceId(),
-    this.beaconProp = beaconProp || 'outline-color',
-    this.beaconStyle = document.getElementsByTagName('head')[0].appendChild(document.createElement('style'));
+    this.beaconConfig = beaconConfig || {
+      property: 'outline-color',
+      from: '#fff',
+      to: '#000'
+    },
+    this.beacon = document.getElementsByTagName('head')[0].appendChild(document.createElement('style'));
 
     this.el.setAttribute('data-class-observee', this.id);
     this.el.addEventListener( transitionend, bind(this.onChange, this) );
@@ -105,7 +131,7 @@
 
     this.currentClass = this.el.getAttribute('class');
 
-    this.beaconStyle.innerHTML = getBeaconStyle(this.id, this.beaconProp, this.currentClass);
+    this.beacon.innerHTML = renderBeacon(this.id, this.beaconConfig, this.currentClass);
   };
 
   /**
@@ -115,7 +141,7 @@
   ClassObserver.prototype.onChange = function(event) {
 
     if (
-      event.propertyName === this.beaconProp &&
+      event.propertyName === this.beaconConfig.property &&
       event.elapsedTime < 0.005 // Leave some extra time, just in case.
     ) {
       this.update();
